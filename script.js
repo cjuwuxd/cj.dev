f99d6678a2c81bf95dbaaebb61ca97fb321160e0
cjuwuxd <cjuwuxd@gmail.com>
2026-04-23 18:38:22 +0800

    added new hamburger bar

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ if(localStorage.getItem("theme")== "dark"){
     document.body.classList.add("dark");
 }
 
-themebtn.addEventListener('click', () => {
+function changeTheme() {
     document.body.classList.toggle("dark");
     console.log("hi")
     if(document.body.classList.contains("dark")){
@@ -16,4 +16,47 @@ themebtn.addEventListener('click', () => {
     }else{
         localStorage.setItem("theme","light");
     }
-});
+}
+
+
+
+/* hamburger sidebar */
+
+var hamburgeractive = false
+
+function openHamburger() {
+
+    if (!hamburgeractive) {
+        document.getElementById("hamburgerbar").style.width = "250px";
+        hamburgeractive = true
+    } else {
+        document.getElementById("hamburgerbar").style.width = "0px";
+        hamburgeractive = false
+    }
+   
+    
+}
+
+function closeHamburger(){
+    document.getElementById("hamburgerbar").style.width = "0px";
+}
+
+/* popup window for settings */
+
+var settingsWindow = document.getElementById("settings-modal");
+var openSettings = document.getElementById("openSettings");
+var span = document.getElementById("close");
+
+openSettings.onclick = function(){
+    settingsWindow.style.display = "block"
+}
+
+span.onclick = function(){
+    settingsWindow.style.display = "none"
+}
+
+window.onclick = function(event){
+    if (event.target==settingsWindow){
+        settingsWindow.style.display = "none";
+    }
+}
